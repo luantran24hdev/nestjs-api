27933441cddfa18c5b62bd411bc5b614377bff1e
luantran <luantran.24hdev@gmail.com>
2022-12-13 09:15:16 +0700

    update cred

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -2,12 +2,15 @@ import { Controller, Get } from '@nestjs/common';
 import { TagService } from './tag.service';
 
 @Controller('tags')
+//unit router api nest
 export class TagController {
   constructor(private readonly tagService: TagService) {}
   @Get()
+  //methods
   async findAll(): Promise<{ tags: string[] }> {
     const tags = await this.tagService.findAll();
     console.log('tags', tags);
+    // => response await of tags
     return {
       tags: tags.map((tag) => tag.name),
     };
