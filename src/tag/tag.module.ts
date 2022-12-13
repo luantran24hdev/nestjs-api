e61d56bc7163689bfc28d989214ea4854b863538
luantran <luantran.24hdev@gmail.com>
2022-12-13 09:29:39 +0700

    update uniti

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -3,6 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TagController } from './tag.controller';
 import { TagEntity } from './tag.entity';
 import { TagService } from './tag.service';
+// module will 3 main content:
+//- prividers (service of controller) -> logic query database (methods findAll, ...)
+//- controllers : init methods (get,post,put,detele) and get response to service
+//- imports : imports module and use logic from outside
 @Module({
   imports: [TypeOrmModule.forFeature([TagEntity])],
   controllers: [TagController],
