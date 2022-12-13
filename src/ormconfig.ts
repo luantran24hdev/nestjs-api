5f181a09a969306a632e5bac693958bf5cc63c88
luantran <luantran.24hdev@gmail.com>
2022-12-13 15:16:02 +0700

    update migration run database in nest

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -8,9 +8,11 @@ const config: ConnectionOptions = {
   password: 'root',
   database: 'postgres',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true,
-  migrations: ['src/migrations/*.ts', 'dist/migrations/*{.ts,.js}'],
-  cli: { migrationsDir: 'src/migrations' },
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
 };
 
 export default config;
